142bc49074b2979afc9297f03a9c572c1de6e511
Cheta-Deore <deorec283gmail.com>
2024-07-30 11:28:36 +0530

    ch1.java and ch2.java  file dev1 i create

---
+++ ch1.java
@@ -0,0 +1,7 @@
+public Class ch1
+{
+	 public static void main(String[] args)
+	{
+	    System.out.println("Skill IT Academy");	
+	}
+}
---
+++ ch2.java
@@ -0,0 +1,8 @@
+public class ch2
+{
+	public static void main(String[] args)
+    {
+    	System.out.println("santosh sir ki class");
+		
+	}
+}
